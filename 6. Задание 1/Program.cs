930cc11d2f38fdf9497188458ad010c27059b129
Yanaa <vaskinayana866@gmail.com>
2026-05-11 10:45:37 +0500

    6Laba

---
+++ 6. Задание 1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace OOP_Task
+{
+    // Базовый класс
+    class LogicalPair
+    {
+        private bool first;
+        private bool second;
+
+        // Конструктор
+        public LogicalPair(bool a, bool b)
+        {
+            first = a;
+            second = b;
+        }
+
+        // Конструктор копирования
+        public LogicalPair(LogicalPair other)
+        {
+            first = other.first;
+            second = other.second;
+        }
+
+        // Импликация
+        public bool Implication()
+        {
+            return !first || second;
+        }
+
+        public override string ToString()
+        {
+            return "First = " + first + ", Second = " + second;
+        }
+
+        public bool GetFirst()
+        {
+            return first;
+        }
+
+        public bool GetSecond()
+        {
+            return second;
+        }
+
+        public void SetValues(bool a, bool b)
+        {
+            first = a;
+            second = b;
+        }
+    }
+
+    // Производный класс
+    class LogicalExpression : LogicalPair
+    {
+        private string name;
+
+        public LogicalExpression(bool a, bool b, string n)
+            : base(a, b)
+        {
+            name = n;
+        }
+
+        // Конструктор копирования
+        public LogicalExpression(LogicalExpression other)
+            : base(other)
+        {
+            name = other.name;
+        }
+
+        // Метод: эквивалентность
+        public bool IsEqual()
+        {
+            return GetFirst() == GetSecond();
+        }
+
+        // Метод: инверсия
+        public void Invert()
+        {
+            SetValues(!GetFirst(), !GetSecond());
+        }
+
+        public override string ToString()
+        {
+            return "Name = " + name + ", " + base.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите first (true/false): ");
+            bool a = bool.Parse(Console.ReadLine());
+
+            Console.Write("Введите second (true/false): ");
+            bool b = bool.Parse(Console.ReadLine());
+
+            // Базовый класс
+            LogicalPair obj = new LogicalPair(a, b);
+            Console.WriteLine(obj);
+            Console.WriteLine("Импликация = " + obj.Implication());
+
+            // Копия
+            LogicalPair copy = new LogicalPair(obj);
+            Console.WriteLine("Копия: " + copy);
+
+            // Производный класс
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            LogicalExpression expr = new LogicalExpression(a, b, name);
+            Console.WriteLine(expr);
+            Console.WriteLine("Эквивалентность = " + expr.IsEqual());
+
+            expr.Invert();
+            Console.WriteLine("После инверсии: " + expr);
+
+            // Копия производного
+            LogicalExpression exprCopy = new LogicalExpression(expr);
+            Console.WriteLine("Копия: " + exprCopy);
+        }
+    }
+}
---
+++ 6. Задание 2, 3/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace OOP_Task2
+{
+    class Money
+    {
+        private uint rubles;
+        private byte kopeks;
+
+        // Конструктор без параметров
+        public Money()
+        {
+            rubles = 0;
+            kopeks = 0;
+        }
+
+        // Конструктор с параметрами
+        public Money(uint rub, byte kop)
+        {
+            rubles = rub;
+
+            if (kop < 100)
+            {
+                kopeks = kop;
+            }
+            else
+            {
+                kopeks = 0;
+            }
+        }
+
+        // Конструктор копирования
+        public Money(Money other)
+        {
+            rubles = other.rubles;
+            kopeks = other.kopeks;
+        }
+
+        // Свойство Rubles
+        public uint GetRubles()
+        {
+            return rubles;
+        }
+
+        public void SetRubles(uint value)
+        {
+            rubles = value;
+        }
+
+        // Свойство Kopeks
+        public byte GetKopeks()
+        {
+            return kopeks;
+        }
+
+        public void SetKopeks(byte value)
+        {
+            if (value < 100)
+            {
+                kopeks = value;
+            }
+        }
+
+        // Метод вычитания копеек
+        public Money SubtractKopeks(uint value)
+        {
+            uint total = rubles * 100 + kopeks;
+
+            if (value >= total)
+            {
+                return new Money(0, 0);
+            }
+
+            total = total - value;
+
+            uint newRubles = total / 100;
+            byte newKopeks = (byte)(total % 100);
+
+            return new Money(newRubles, newKopeks);
+        }
+
+        // Перегрузка оператора -
+        public static Money operator -(Money money, uint value)
+        {
+            return money.SubtractKopeks(value);
+        }
+
+        public override string ToString()
+        {
+            return rubles + " руб. " + kopeks + " коп.";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите рубли: ");
+            uint rub = uint.Parse(Console.ReadLine());
+
+            Console.Write("Введите копейки (0-99): ");
+            byte kop = byte.Parse(Console.ReadLine());
+
+            Money m1 = new Money(rub, kop);
+
+            Console.WriteLine("\nОбъект:");
+            Console.WriteLine(m1);
+
+            // Конструктор копирования
+            Money copy = new Money(m1);
+            Console.WriteLine("Копия:");
+            Console.WriteLine(copy);
+
+            Console.Write("\nСколько копеек вычесть? ");
+            uint x = uint.Parse(Console.ReadLine());
+
+            // Метод
+            Money result1 = m1.SubtractKopeks(x);
+            Console.WriteLine("Через метод: " + result1);
+
+            // Перегруженный оператор
+            Money result2 = m1 - x;
+            Console.WriteLine("Через оператор - : " + result2);
+        }
+    }
+}
